b58b596eb6e529b53b623a822c44f2e39894eb02
Susmita Sinha <121500105+susmita588@users.noreply.github.com>
2025-09-13 16:06:06 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import "../css/home.css"
 const Home = () => {
-  return (
-    <div>
-      <h1>This is Our React project</h1>
-    </div>
-  )
+  return<>
+    <h1>
+      this is my page</h1>
+      <div className='bg'>
+        
+      </div>
+  </>
 }
 
 export default Home
